35c7306d657ddc431f716de824a054a385530664
Alex Kon <wildnifer@narod.ru>
2019-05-09 22:52:12 +0300

    create BO PostGenerator

---
+++ SeleniumWD/BO/PostGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWD.BO
+{
+    public static class PostGenerator
+    {
+        public static object[] DataTest = new object[]
+        {
+            new Post("Test", "http://localhost:3030/admin", "Test testing.")        
+        };
+
+    }
+}
