306d554b3c7c43a8b094fb35610e136e3b211a04
Alex Kon <wildnifer@narod.ru>
2019-05-09 22:43:23 +0300

    create BO Post

---
+++ SeleniumWD/BO/Post.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWD.BO
+{
+    class Post
+    {
+        private string title;
+        private string url;
+        private string body;
+
+        public Post(string title, string url, string body)
+        {
+            this.title = title;
+            this.url = url;
+            this.body = body;
+        }
+        public string GetTitle()
+        {
+            return title;   
+        }
+        public string GetUrl()
+        {
+            return url;
+        }
+        public string GetBody()
+        {
+            return body;
+        }
+    }
+}
