3b4671e60ff6a964336f335811e9077afc7616bd
Alex Kon <wildnifer@narod.ru>
2019-05-09 22:14:16 +0300

    replace PosteEditorPage, create LoginFunctions

---
+++ SeleniumWD/BusinessFunctions/LoginFunctions.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using SeleniumWD.BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWD.BusinessFunctions
+{
+    class LoginFunctions
+    {
+        public IWebDriver driver;
+        public LoginFunctions(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Login(User user)
+        {
+            new LoginPage(driver).Login(user.getLogin(), user.getPassword());
+        }
+    }
+}
