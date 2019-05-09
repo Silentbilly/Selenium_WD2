c1ada963bd29a967979fb0d7945315456894303d
Alex Kon <wildnifer@narod.ru>
2019-05-09 21:56:51 +0300

    add TestUsersGenerator, User

---
+++ SeleniumWD/BO/TestUsersGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWD.BO
+{
+    class TestUsersGenerator
+    {
+        public static User GenerateValidUser()
+        {
+            return new User("admin", "admin");
+        }
+    }
+}
---
+++ SeleniumWD/BO/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWD.BO
+{
+    class User
+    {
+        private string login;
+        private string password;
+        public User(string login, string password)
+        {
+            this.login = login;
+            this.password = password;
+        }
+        
+        public string getLogin()
+        {
+            return login;
+        }
+
+        public string getPassword()
+        {
+            return password;
+        }
+    }
+}
